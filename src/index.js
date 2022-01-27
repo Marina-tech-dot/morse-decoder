06954ee9ec379b8f64ab994ec3b9916baeacd750
Marina-tech-dot <marina.juriskonsult.gmail.com>
2022-01-27 19:47:59 +0100

    feat: solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ': ' ',
 };
 
+
+const StringSimbol = {
+    '10': '.',
+    '11': '-',
+    '**': ' ',
+    '00': '',
+}
+
+
 function decode(expr) {
-    // write your solution here
+
+    let arr = []
+
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+
+    arr = arr
+        .map(el => {
+
+            let arr2 = []
+
+            for (let i = 0; i < el.length; i += 2) {
+                arr2.push(el.slice(i, i + 2))
+            }
+            return arr2
+        })
+        .map(elem => elem.map(item => StringSimbol[item]))
+        .map(item => item.join(""))
+        .map(item => MORSE_TABLE[item]).join("")
+
+        return arr
+
 }
 
 module.exports = {
